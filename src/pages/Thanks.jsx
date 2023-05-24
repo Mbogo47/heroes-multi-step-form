d32da8f1c410a9ad3c377e38d069cc930929fc52
judiciousmurich <murichjudicious45@gmail.com>
2023-05-24 23:36:52 +0300

    Added stylings

--- src/pages/Thanks.jsx
+++ src/pages/Thanks.jsx
@@ -7,7 +7,7 @@ function Thanks() {
         <div className="thanks-container">
             <img src={iconthankyou} />
             <h3>Thank you!</h3>
-            <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us @support@loremgaming.com</p>
+            <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us @support@loremgaming.com.</p>
         </div>
     )
 }
